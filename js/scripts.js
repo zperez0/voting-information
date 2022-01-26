edd90ac6307056022d1fd36d86ebda0f9300ab48
Yazmin Perez <zoeperez333@gmail.com>
2022-01-26 09:48:11 -0800

    remove age form section

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
   const age = parseInt(prompt("How old are you?"));
 
-  if (age >=21) {
-    $(#voteInfo).show();
+  if (age >=18) {
+    $("#voteInfo").show();
   }else{
     $("#under-18").show();
   }
 });
+
+// $(document).ready(function() {
+//   const over21 = confirm("Are you over 21? Click OK for yes or Cancel for no.");
+
+//   if (over21) {
+//     $('#drinks').show();
+//   } else {
+//     $('#under-21').show();
+//   }
+// });
